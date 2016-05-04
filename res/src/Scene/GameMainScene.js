1194d2582c3e2416b1b33b342be626bb1bf0b06f
oiy37215 <414755548@qq.com>
2016-05-04 15:03:45 +0800

    dsfasfdasdf

---
+++ res/src/Scene/GameMainScene.js
@@ -0,0 +1,96 @@
+
+var gameMainLayer = cc.Layer.extend({
+	sprite:null,
+	sceneManage : null,
+	public : null,
+	ctor:function () {
+		//////////////////////////////
+		// 1. super init first
+		this._super();
+
+		/////////////////////////////
+		// 2. add a menu item with "X" image, which is clicked to quit the program
+		//    you may modify it.
+		// ask the window size
+		var size = cc.winSize;
+		this.sceneManage = new SceneManage();
+		this.public = new publicAPI(this);
+		cc.log(jsb.fileUtils.getSearchPaths());
+		//bg
+		this.public.showBg();
+		MW.MAIN_MENU_X = size.width/2;
+		MW.MAIN_MENU_Y = size.height/2+151;
+		MW.MENU_X = size.width/2-140;
+		MW.MENU_Y = size.height-150;
+		MW.THREE_MENU_X = size.width/2-160;
+		//logo
+		var spr_log = new cc.Sprite(res.logo_png);
+		spr_log.setPosition(size.width/2,size.height-100);
+		this.addChild(spr_log);
+		//词语
+		var ciyu_menuItem = new cc.MenuItemImage(res.blue_ciyu_png, res.red_ciyu_png, this.onBackCallback, this);
+		ciyu_menuItem.setScale(MW.MAIN_MENU_SCALE_NUM);
+		ciyu_menuItem.setPosition(MW.MAIN_MENU_X, MW.MAIN_MENU_Y);
+		ciyu_menuItem.setTag(1);
+		//句子
+		var juzi_menuItem = new cc.MenuItemImage(res.blue_juzi_png, res.red_juzi_png, this.onBackCallback, this);
+		juzi_menuItem.setScale(MW.MAIN_MENU_SCALE_NUM);
+		juzi_menuItem.setPosition(MW.MAIN_MENU_X, MW.MAIN_MENU_Y-MW.MENUITEM_HEIGHT*MW.MAIN_MENU_SCALE_NUM-MW.MAIN_MENU_SPACE);
+		juzi_menuItem.setTag(2);
+		//段子
+		var duanzi_menuItem = new cc.MenuItemImage(res.blue_duanzi_png, res.red_duanzi_png, this.onBackCallback, this);
+		duanzi_menuItem.setScale(MW.MAIN_MENU_SCALE_NUM);
+		duanzi_menuItem.setPosition(MW.MAIN_MENU_X, MW.MAIN_MENU_Y-MW.MENUITEM_HEIGHT*MW.MAIN_MENU_SCALE_NUM*2-MW.MAIN_MENU_SPACE*2);
+		duanzi_menuItem.setTag(3);
+		//文章
+		var wenzhang_menuItem = new cc.MenuItemImage(res.blue_wenzhang_png, res.red_wenzhang_png, this.onBackCallback, this);
+		wenzhang_menuItem.setScale(MW.MAIN_MENU_SCALE_NUM);
+		wenzhang_menuItem.setPosition(MW.MAIN_MENU_X, MW.MAIN_MENU_Y-MW.MENUITEM_HEIGHT*MW.MAIN_MENU_SCALE_NUM*3-MW.MAIN_MENU_SPACE*3);
+		wenzhang_menuItem.setTag(4);
+
+		var zibian_menuItem = new cc.MenuItemImage(res.zibian_blue_png, res.zibian_red_png, this.onBackCallback, this);
+		zibian_menuItem.setScale(MW.MAIN_MENU_SCALE_NUM);
+		zibian_menuItem.setPosition(MW.MAIN_MENU_X, MW.MAIN_MENU_Y-MW.MENUITEM_HEIGHT*MW.MAIN_MENU_SCALE_NUM*4-MW.MAIN_MENU_SPACE*4);
+		zibian_menuItem.setTag(5);
+		
+		var menu = new cc.Menu(ciyu_menuItem,juzi_menuItem,duanzi_menuItem,wenzhang_menuItem,zibian_menuItem);
+		menu.setPosition(0, 0);
+		this.addChild(menu);
+
+		var spr_hello = new cc.Sprite(res.hello_png);
+		spr_hello.setPosition(300,300);
+		this.addChild(spr_hello);
+	},
+	onBackCallback : function(sender){
+		switch(sender.getTag()){
+		case 1:
+			this.public.setSpeakingSpedd("5");
+			this.sceneManage.TrunCiyuScene(1, false);
+			break;
+		case 2:
+			this.sceneManage.TrunJuZiScene(1, false);
+			break;
+		case 3:
+			MW.CIYU_SELECT = 1;
+			this.sceneManage.TrunWenZhangScene(1, false);
+			break;
+		case 4:
+			MW.CIYU_SELECT = 2;
+			this.sceneManage.TrunWenZhangScene(1, false);
+			break;
+		case 5:
+			this.sceneManage.TrunZiBianScene(1,false);
+			break;
+		}
+	}
+	
+	});
+
+var gameMainScene = cc.Scene.extend({
+	onEnter:function () {
+		this._super();
+		var layer = new gameMainLayer();
+		this.addChild(layer);
+	}
+});
+
---
+++ res/src/resource.js
@@ -0,0 +1,48 @@
+var res = {
+    XiaoWu_jpg : "res/xiaowu.jpg",
+    blue_ciyu_png : "res/blue_ciyu.png",
+    blue_duanzi_png : "res/blue_duanzi.png",
+    blue_juzi_png : "res/blue_juzi.png",
+    blue_wenzhang_png : "res/blue_wenzhang.png",
+    red_ciyu_png : "res/red_ciyu.png",
+    red_duanzi_png : "res/red_duanzi.png",
+    red_juzi_png : "res/red_juzi.png",
+    red_wenzhang_png : "res/red_wenzhang.png",
+    jiantou_png : "res/jiantou.png",
+    blue_animal_png : "res/blue_animal.png",
+    blue_city_png : "res/blue_city.png",
+    blue_plant_png : "res/blue_plant.png",
+    blue_subway_png : "res/blue_subway.png",
+    blue_vihicle_png : "res/blue_vihicle",
+    red_animal_png : "res/red_animal.png",
+    red_city_png : "res/red_city.png",
+    red_plant_png : "res/red_plant.png",
+    red_subway_png : "res/red_subway",
+    red_vihicle_png : "res/red_vihicle.png",
+    one_blue_png : "res/one_blue.png",
+    one_red_png : "res/one_red.png",
+    play_png : "res/play.png",
+    jiantou_red_png : "res/jiantou_red.png",
+    play_red_png : "res/play_red.png",
+    red_car_png : "res/car_red.png",
+    blue_car_png : "res/car_blue.png",
+    set_png : "res/settings.png",
+    dialog_png : "res/dialog.png",
+    stop_png : "res/stop.png",
+    zibian_blue_png : "res/zibian_blue.png",
+    zibian_red_png : "res/zibian_red.png",
+    add_blue_png : "res/add.png",
+    content_frame_png : "res/contentFrame.png",
+    save_blue_png : "res/save_blue.png",
+    save_red_png : "res/save_red.png",
+    blue_frame_png : "res/blue_frame.png",
+    red_frame_png : "res/red_frame.png",
+    trim_png : "res/trim.png",
+    logo_png : "res/logo.png",
+    hello_png : "res/HelloWorld.png"
+};
+
+var g_resources = [];
+for (var i in res) {
+    g_resources.push(res[i]);
+}
